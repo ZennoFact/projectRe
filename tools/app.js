a23beea0eaefe136c7d3c2266e62ce1b9369fe79
ZennoFact <zenno04.g@gmail.com>
2015-12-26 02:58:38 +0900

    [Modify] Output json file

--- tools/app.js
+++ tools/app.js
@@ -1,4 +1,5 @@
 var CSV = require("comma-separated-values");
+var fs = require('fs');
 
 var data = '\
 title,path,tag1,tag2,tag3\r\n\
@@ -9,3 +10,5 @@ js,./js.md,web,front,program\r\n\
 
 var result = new CSV(data, { header: true }).parse();
 console.log(result);
+
+fs.writeFile('data.json', JSON.stringify(result, null, '  '));
