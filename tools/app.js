86a40f788590de7462809b625535f4f7783471f7
ZennoFact <zenno04.g@gmail.com>
2015-12-26 02:48:24 +0900

    Tweak: Sample csv data.

---
+++ tools/app.js
@@ -0,0 +1,11 @@
+var CSV = require("comma-separated-values");
+
+var data = '\
+title,path,tag1,tag2,tag3\r\n\
+html,./html.md,web,front,markup\r\n\
+PHP,./php.md,web,serverside,\r\n\
+js,./js.md,web,front,program\r\n\
+';
+
+var result = new CSV(data, { header: true }).parse();
+console.log(result);
