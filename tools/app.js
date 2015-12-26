a55da4133300ac8a1f946c7e937a023c74f12827
ZennoFact <zenno04.g@gmail.com>
2015-12-26 10:27:21 +0900

    [Modify] Read csv file. But can't choose any file.

--- tools/app.js
+++ tools/app.js
@@ -1,14 +1,11 @@
 var CSV = require("comma-separated-values");
 var fs = require('fs');
 
-var data = '\
-title,path,tag1,tag2,tag3\r\n\
-html,./html.md,web,front,markup\r\n\
-PHP,./php.md,web,serverside,\r\n\
-js,./js.md,web,front,program\r\n\
-';
+var buffer = fs.readFileSync('./database.csv');
+
+var data = buffer.toString();
 
 var result = new CSV(data, { header: true }).parse();
 console.log(result);
 
-fs.writeFile('data.json', JSON.stringify(result, null, '  '));
+fs.writeFile('../data.json', JSON.stringify(result, null, '  '));
