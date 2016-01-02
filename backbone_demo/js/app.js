02a9d51f30a1129b9085aa98dffef2e424e4a0d3
ZennoFact <zenno04.g@gmail.com>
2016-01-02 22:53:55 +0900

    [Add] Backbone.js-sample: TodoApp

---
+++ backbone_demo/js/app.js
@@ -0,0 +1,11 @@
+// 全体的な初期化のコードをここに記載
+var app = app || {};
+// キーコードを保存しておく
+var ENTER_KEY = 13;
+
+$(function () {
+	'use strict';
+
+	// kick things off by creating the `App`
+	new app.AppView();
+});
